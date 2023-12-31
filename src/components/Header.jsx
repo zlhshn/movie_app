3c8ae26d33902c880414426232fc581126f0ceef
zlhshn <zsahin4401@gmail.com>
2023-12-31 11:05:30 +0300

    responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="flex justify-center gap-4 p-6 cursor-pointer bg-[ rgba(34, 54, 71, 0.997)] text-[#080808] dark:text-[#acaaaa]">
+    <div className="flex justify-center flex-col items-center sm:flex-row gap-4 p-6 cursor-pointer bg-[ rgba(34, 54, 71, 0.997)] text-[#080808] dark:text-[#acaaaa]">
       <NavLink className="p-1.5" to="/">MOVİES</NavLink>
       <NavLink className="p-1.5" to="/topmovie">TOP-MOVİES</NavLink>
       <NavLink className="p-1.5" to="/tv">TV SERİES</NavLink>
